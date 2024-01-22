6e07dfc2efeda6a954ac0044251057afd542af67
MichaelHornstei <12235028-MichaelHornstei@users.noreply.replit.com>
2024-01-22 07:10:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def calming_mantra():
+  name = input('What is your name?\n')
+  print(f"{name}, gently bring your attention to the present moment.")
+
+if __name__ == "__main__": 
+  calming_mantra() 
